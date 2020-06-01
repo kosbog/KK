2acb809a17744a16d5ac9b6181dec28b48b5de51
kosbog <badykos@gmail.com>
2020-06-01 16:56:13 +0300

    delete logo frame

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
 import { observer } from "mobx-react-lite";
-import { Image } from "semantic-ui-react";
+import { Image, Transition } from "semantic-ui-react";
 import logo from "../assets/images/logo.22f8373f.jpg";
 import { CustomDivider } from "./Divider";
 import { Link } from "react-router-dom";
 
 export const Logo = observer(() => {
+  const [isVisible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    setVisible(true);
+  }, [isVisible]);
   return (
     <>
-      <div className="logo-wrapper">
-        <div className="logo-container">
-          <div className="logo-image">
-            <Link to={"/"}>
-              <Image src={logo} fluid />
-            </Link>
+      <Transition visible={isVisible} animation="fade" duration={600}>
+        <div className="logo-wrapper">
+          <div className="logo-container">
+            <div className="logo-image">
+              <Link to={"/"}>
+                <Image src={logo} fluid />
+              </Link>
+            </div>
           </div>
+          {/* <* className="logo-frame" />*/}
         </div>
-        <div className="logo-frame"></div>
-      </div>
+      </Transition>
       <CustomDivider />
     </>
   );
